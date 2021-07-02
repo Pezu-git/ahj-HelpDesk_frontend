96e2e9cc558d698ef0f2dec2e192c9ed9741f809
Pezu-git <artem_nikolaev69@mail.ru>
2021-07-02 08:17:05 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,44 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-plusplus */
+/* eslint-disable linebreak-style */
+
+const board = document.createElement('div');
+board.classList.add('board');
+document.body.appendChild(board);
+
+const numArr = [];
+// const number = Math.floor(Math.random() * (max - min));
+
+for (let i = 0; i < 16; i++) {
+  const newMap = document.createElement('div');
+  newMap.classList.add('map');
+  board.appendChild(newMap);
+  const newImgDiv = document.createElement('div');
+  newImgDiv.classList.add('img');
+  newMap.appendChild(newImgDiv);
+  const imgDiv = new Image();
+  imgDiv.src = 'https://github.com/netology-code/ahj-homeworks/blob/simplification/dom/pic/goblin.png?raw=true';
+  imgDiv.classList.add('goblin');
+  newImgDiv.appendChild(imgDiv);
+}
+export default function appear(maxMap) {
+  const min = Math.ceil(0);
+  const max = Math.floor(maxMap);
+  const number = Math.floor(Math.random() * (max - min));
+  const goblin = document.querySelectorAll('.goblin');
+  const active = document.querySelector('.active');
+  while (numArr[0] === number && number > min) {
+    goblin[number - 1].classList.add('active');
+    numArr[0] = number - 1;
+  }
+  while (numArr[0] === number && number < max - 1) {
+    goblin[number + 1].classList.add('active');
+    numArr[0] = number + 1;
+  }
+  goblin[number].classList.add('active');
+  numArr[0] = number;
+  if (active) {
+    active.classList.remove('active');
+  }
+}
+setInterval(() => appear(16), 600);
